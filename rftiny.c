7f84405336b64b605291e2cb8245e174d24a0c5a
VBrawl <konstantosjim@gmail.com>
2025-04-30 10:37:38 +0300

    Changed rftiny_add_buf to be a macro and added preamble

--- rftiny.c
+++ rftiny.c
@@ -1,30 +1,33 @@
 #include "rftiny.h"
 
-void rftiny_add_buf(uint8_t bit, rftiny_buf *buf) {
-	*buf = (*buf << 1) | (bit & 1);
-}
-
 rftiny_buf rftiny_find_packet() {
 	rftiny_buf buf = 0;
 
 	while(1) {
 		while(buf != SYNGID)
-			rftiny_add_buf(rftiny_get_bit(), &buf);
+			rftiny_add_buf(rftiny_get_bit(), buf);
 
 		/* read CODE */
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
-		rftiny_add_buf(rftiny_get_bit(), &buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
+		rftiny_add_buf(rftiny_get_bit(), buf);
 		return buf;
 	}
 }
 
+void rftiny_put_preamble() {
+	for(uint8_t i = 0; i < 12; i++) {
+		rftiny_put_buf(0b10101010);
+	}
+}
+
 void rftiny_put_packet(rftiny_buf data) {
+	rftiny_put_preamble();
 	rftiny_put_buf(SYNGID);
 	rftiny_put_buf(data);
 }
--- rftiny.h
+++ rftiny.h
@@ -5,12 +5,13 @@
 
 typedef uint8_t rftiny_buf;
 
-#ifndef SYNGID
-#define SYNGID 0b11010111
-#endif
+#define SYNGID 0b11101101
 
-void rftiny_add_buf(uint8_t bit, rftiny_buf *buf);
+#define rftiny_add_buf(bit, buf) (buf = (buf << 1) | (bit & 1))
+
+void rftiny_put_preamble();
 rftiny_buf rftiny_find_packet();
+void rftiny_put_asksyn();
 void rftiny_put_packet(rftiny_buf data);
 uint8_t rftiny_get_bit();
 void rftiny_put_buf(rftiny_buf buf);
